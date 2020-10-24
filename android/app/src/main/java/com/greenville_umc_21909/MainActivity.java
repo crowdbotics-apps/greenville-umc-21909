23b64b4b2a8bd13a61163af2415a42b8d7cf937b
Crowdbotics <team@crowdbotics.com>
2020-10-24 14:43:58 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/greenville_umc_21909/MainActivity.java
@@ -0,0 +1,15 @@
+package com.greenville_umc_21909;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "greenville_umc_21909";
+    }
+}
---
+++ backend/greenville_umc_21909/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for greenville_umc_21909 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'greenville_umc_21909.settings')
+
+application = get_wsgi_application()
